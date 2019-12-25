98de92dcf68a77088475954b3a714e0e1c14b0c9
herbert.blaschek@brz.gv.at <Trebreh$$44>
2019-12-25 19:41:47 +0100

    first

---
+++ src/main/java/at/hb/photo/photomanager/PhotomanagerApplication.java
@@ -0,0 +1,13 @@
+package at.hb.photo.photomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotomanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotomanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/hb/photo/photomanager/PhotomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.hb.photo.photomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotomanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
